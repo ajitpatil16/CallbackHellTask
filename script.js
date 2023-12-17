d412f6ba4d764aa6703f86c2b6927aea3a1922a6
Ajit Patil <90565508+ajitpatil16@users.noreply.github.com>
2023-12-17 22:11:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const h1=document.querySelector("#h1");
+console.log(h1);
+
+setTimeout(()=>{
+    h1.innerHTML="10";
+    setTimeout(()=>{
+        h1.innerHTML="9";
+        setTimeout(()=>{
+            h1.innerHTML="8";
+            setTimeout(()=>{
+                h1.innerHTML="7";
+                setTimeout(()=>{
+                    h1.innerHTML="6";
+                    setTimeout(()=>{
+                        h1.innerHTML="5";
+                        setTimeout(()=>{
+                            h1.innerHTML="4";
+                            setTimeout(()=>{
+                                h1.innerHTML="3";
+                                setTimeout(()=>{
+                                    h1.innerHTML="2";
+                                    setTimeout(()=>{
+                                        h1.innerHTML="1";
+                                        setTimeout(()=>{
+                                            h1.innerHTML="Happy Independence Day";
+                                            
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
+
+setTimeout();
